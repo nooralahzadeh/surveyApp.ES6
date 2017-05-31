40bf3b7f0df27cd9d4fcbbcf75c81bcfd86191f4
Farhad Administrator <farhadadm@mac-tb-netinstalldongle.local>
2017-05-31 12:32:39 +0200

    config firbase

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -5,7 +5,9 @@ try {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
